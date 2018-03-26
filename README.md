9cac88a32ab0a5b74397c2787b9193be309bfa0e
Tran_Binh <tranbinhbak@gmail.com>
2018-03-26 21:35:45 +0700

    The final

--- README.md
+++
@@ -1,1 +0,0 @@
-# quanlydoan_v2
--- db_conect.php
+++
@@ -1,6 +0,0 @@
-<?php
- $conn = mysqli_connect("localhost","root",""); 
- mysqli_select_db($conn,"quanly");
- // mysqli_set_charset($conn, "SET NAMES 'utf8'");
- mysqli_query($conn,"SET NAMES 'utf8'");
- ?>﻿
